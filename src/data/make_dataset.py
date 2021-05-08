553340960444d7a92491d82e6271200b335d52ef
romansnetkov <snetkov.roman@gmail.com>
2021-05-08 15:59:32 +0700

    add dataset make

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,7 +1,20 @@
+from typing import Tuple
+
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 def read_data(path: str) -> pd.DataFrame:
     data = pd.read_csv(path)
     return data
 
+
+def split_train_val_data(
+        data: pd.DataFrame,
+        params
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    train_data, validation_data = train_test_split(
+        data,
+        test_size=params.val_size,
+    )
+    return train_data, validation_data
